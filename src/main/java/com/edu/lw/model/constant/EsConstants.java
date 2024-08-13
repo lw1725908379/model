495d68817991150eac9d93ef1cb5684e6b39c5f4
lw1725908379 <lw1725908379@gmail.com>
2024-08-13 17:31:01 +0800

    延迟队列实现预约充电功能、Kafka、es搜索功能

---
+++ src/main/java/com/edu/lw/model/constant/EsConstants.java
@@ -0,0 +1,13 @@
+package com.edu.lw.model.constant;
+
+/**
+ * @author 17259
+ * @create 2024-08-05 21:09
+ */
+public class EsConstants {
+    // Elasticsearch 索引名称
+    public static final String ES_INFO_STATION_INDEX = "es_info_station";
+
+    // Elasticsearch 批量导入默认分页大小
+    public static final int ES_PAGE_SIZE = 2000;
+}
